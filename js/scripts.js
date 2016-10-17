cbc329364cc2c12e7fc387e1e1ed7222848c3882
Brad Copenhaver and Kevin Finley <kevin@>
2016-10-17 14:24:25 -0700

    add reverse letters

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,14 @@
 var sentence = prompt("Please type a sentence!");
 
 var firstLastSlice = function(sentenceInput) {
-  return sentenceInput.slice(0, 1) + sentenceInput.slice(-1);
+  var result = sentenceInput.slice(0, 1) + sentenceInput.slice(-1);
+  return result.toUpperCase();
 }
-var result = firstLastSlice(sentence);
-alert(result.toUpperCase());
+
+var reverse = function(sentenceInput) {
+  var result = sentenceInput.slice(-1) + sentenceInput.slice(0, 1);
+  return result.toUpperCase();
+}
+
+alert(firstLastSlice(sentence));
+alert(reverse(sentence));
